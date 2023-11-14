0d39b0ecb23ab25a511d4f3d0f6610a3fffc32fe
Philip Rehberger <me@philiprehberger.com>
2023-11-13 22:29:05 -0600

    Release v0.2.0

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 [![Tests](https://github.com/philiprehberger/py-json-diff/actions/workflows/publish.yml/badge.svg)](https://github.com/philiprehberger/py-json-diff/actions/workflows/publish.yml)
 [![PyPI version](https://img.shields.io/pypi/v/philiprehberger-json-diff.svg)](https://pypi.org/project/philiprehberger-json-diff/)
+[![GitHub release](https://img.shields.io/github/v/release/philiprehberger/py-json-diff)](https://github.com/philiprehberger/py-json-diff/releases)
+[![Last updated](https://img.shields.io/github/last-commit/philiprehberger/py-json-diff)](https://github.com/philiprehberger/py-json-diff/commits/main)
 [![License](https://img.shields.io/github/license/philiprehberger/py-json-diff)](LICENSE)
+[![Bug Reports](https://img.shields.io/github/issues/philiprehberger/py-json-diff/bug)](https://github.com/philiprehberger/py-json-diff/issues?q=is%3Aissue+is%3Aopen+label%3Abug)
+[![Feature Requests](https://img.shields.io/github/issues/philiprehberger/py-json-diff/enhancement)](https://github.com/philiprehberger/py-json-diff/issues?q=is%3Aissue+is%3Aopen+label%3Aenhancement)
 [![Sponsor](https://img.shields.io/badge/sponsor-GitHub%20Sponsors-ec6cb9)](https://github.com/sponsors/philiprehberger)
 
-Readable JSON/dict comparison with colorized terminal output.
+Readable JSON comparison with colorized terminal output.
 
 ## Installation
 
@@ -34,6 +38,8 @@ summary = diff_summary(changes)
 ### Nested Comparison
 
 ```python
+from philiprehberger_json_diff import diff
+
 old = {"user": {"name": "Alice", "settings": {"theme": "dark"}}}
 new = {"user": {"name": "Alice", "settings": {"theme": "light"}}}
 
@@ -44,15 +50,62 @@ changes = diff(old, new)
 ### Ignore Paths
 
 ```python
+from philiprehberger_json_diff import diff
+
 changes = diff(old, new, ignore={"user.settings.theme"})
+```
+
+### Wildcard Ignore Patterns
+
+```python
+from philiprehberger_json_diff import diff
+
+old = {"a": {"metadata": {"ts": 1}}, "b": {"metadata": {"ts": 2}}}
+new = {"a": {"metadata": {"ts": 99}}, "b": {"metadata": {"ts": 99}}}
+
+# Ignore metadata fields at any depth
+changes = diff(old, new, ignore={"*.metadata.*"})
+```
+
+### Structural Diff Mode
+
+```python
+from philiprehberger_json_diff import diff
+
+old = {"a": 1, "b": "hello", "c": 3}
+new = {"a": "one", "b": "world", "d": 4}
+
+result = diff(old, new, mode="structural")
+result.key_additions   # [Change(path='d', ...)]
+result.key_removals    # [Change(path='c', ...)]
+result.value_changes   # [Change(path='b', ...)]
+result.type_changes    # [Change(path='a', ...)]
+```
+
+### Apply Patch
+
+```python
+from philiprehberger_json_diff import diff, apply_patch
+
+old = {"name": "Alice", "age": 30}
+new = {"name": "Bob", "age": 31}
+
+changes = diff(old, new)
+result = apply_patch(old, changes)
+# result == {"name": "Bob", "age": 31}
 ```
 
 ## API
 
-- `diff(old, new, ignore=None)` — Compare two dicts, returns list of `Change` objects
-- `format_diff(changes, color=True)` — Format changes as readable string with optional ANSI colors
-- `diff_summary(changes)` — Return dict with counts by change type
-
+| Function / Class | Description |
+|------------------|-------------|
+| `diff(a, b, ignore, mode)` | Compare two dicts/lists, returns list of `Change` objects or `StructuralDiff` |
+| `format_diff(changes, color)` | Format changes as readable string with optional ANSI colors |
+| `diff_summary(changes)` | Return dict with counts by change type |
+| `apply_patch(target, changes)` | Apply a diff result as a patch to reconstruct the modified object |
+| `Change` | Dataclass with `path`, `change_type`, `old_value`, `new_value` |
+| `ChangeType` | Enum: `ADDED`, `REMOVED`, `MODIFIED`, `UNCHANGED` |
+| `StructuralDiff` | Dataclass with `key_additions`, `key_removals`, `value_changes`, `type_changes` |
 
 ## Development
 
@@ -61,6 +114,13 @@ pip install -e .
 python -m pytest tests/ -v
 ```
 
+## Support
+
+If you find this package useful, consider giving it a star on GitHub — it helps motivate continued maintenance and development.
+
+[![LinkedIn](https://img.shields.io/badge/Philip%20Rehberger-LinkedIn-0A66C2?logo=linkedin)](https://www.linkedin.com/in/philiprehberger)
+[![More packages](https://img.shields.io/badge/more-open%20source%20packages-blue)](https://philiprehberger.com/open-source-packages)
+
 ## License
 
-MIT
+[MIT](LICENSE)
--- src/philiprehberger_json_diff/__init__.py
+++ src/philiprehberger_json_diff/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import copy
+import fnmatch
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any
@@ -10,9 +12,11 @@ from typing import Any
 __all__ = [
     "ChangeType",
     "Change",
+    "StructuralDiff",
     "diff",
     "format_diff",
     "diff_summary",
+    "apply_patch",
 ]
 
 
@@ -35,25 +39,49 @@ class Change:
     new_value: Any = None
 
 
+@dataclass(frozen=True)
+class StructuralDiff:
+    """Diff result separated into structural vs value changes."""
+
+    key_additions: list[Change]
+    key_removals: list[Change]
+    value_changes: list[Change]
+    type_changes: list[Change]
+
+
 def diff(
     a: Any,
     b: Any,
     ignore: set[str] | list[str] | None = None,
+    mode: str = "full",
     _path: str = "",
-) -> list[Change]:
+) -> list[Change] | StructuralDiff:
     """Compare two JSON-compatible objects and return a list of changes.
 
     Args:
         a: The original object.
         b: The new object.
-        ignore: Set of top-level or dot-path keys to ignore.
+        ignore: Set of dot-path keys or wildcard patterns (e.g. ``*.metadata.*``)
+            to ignore.
+        mode: ``"full"`` (default) returns a flat list of changes.
+            ``"structural"`` returns a :class:`StructuralDiff` separating
+            key additions/removals from value/type changes.
         _path: Internal path prefix for recursion.
 
     Returns:
-        List of Change objects describing every difference.
+        List of Change objects (``mode="full"``) or a :class:`StructuralDiff`
+        (``mode="structural"``).
     """
     ignore_set = set(ignore) if ignore else set()
-    return _diff_values(a, b, _path, ignore_set)
+    changes = _diff_values(a, b, _path, ignore_set)
+
+    if mode == "structural":
+        return _to_structural(changes)
+    return changes
+
+
+def _has_wildcard(pattern: str) -> bool:
+    return "*" in pattern or "?" in pattern or "[" in pattern
 
 
 def _should_ignore(path: str, ignore: set[str]) -> bool:
@@ -61,8 +89,18 @@ def _should_ignore(path: str, ignore: set[str]) -> bool:
         return False
     if path in ignore:
         return True
-    parts = path.rsplit(".", 1)
-    return parts[-1] in ignore if len(parts) > 1 else False
+
+    for pattern in ignore:
+        if _has_wildcard(pattern):
+            if fnmatch.fnmatch(path, pattern):
+                return True
+        else:
+            # Legacy behaviour: match trailing key name
+            parts = path.rsplit(".", 1)
+            if len(parts) > 1 and parts[-1] == pattern:
+                return True
+
+    return False
 
 
 def _diff_values(
@@ -122,6 +160,32 @@ def _diff_lists(
     return changes
 
 
+def _to_structural(changes: list[Change]) -> StructuralDiff:
+    """Split a flat change list into structural categories."""
+    key_additions: list[Change] = []
+    key_removals: list[Change] = []
+    value_changes: list[Change] = []
+    type_changes: list[Change] = []
+
+    for change in changes:
+        if change.change_type == ChangeType.ADDED:
+            key_additions.append(change)
+        elif change.change_type == ChangeType.REMOVED:
+            key_removals.append(change)
+        elif change.change_type == ChangeType.MODIFIED:
+            if type(change.old_value) is not type(change.new_value):
+                type_changes.append(change)
+            else:
+                value_changes.append(change)
+
+    return StructuralDiff(
+        key_additions=key_additions,
+        key_removals=key_removals,
+        value_changes=value_changes,
+        type_changes=type_changes,
+    )
+
+
 def format_diff(changes: list[Change], color: bool = True) -> str:
     """Format a list of changes as a human-readable string.
 
@@ -175,3 +239,97 @@ def diff_summary(changes: list[Change]) -> dict[str, int]:
     for change in changes:
         summary[change.change_type.value] += 1
     return summary
+
+
+def apply_patch(target: Any, changes: list[Change]) -> Any:
+    """Apply a diff result as a patch to reconstruct the modified object.
+
+    Takes a target object and a list of changes (as returned by
+    ``diff(a, b)``), then applies the additions, removals, and
+    modifications to produce the patched object.
+
+    Args:
+        target: The base object to patch (will not be mutated).
+        changes: List of Change objects to apply.
+
+    Returns:
+        A new object with all changes applied.
+    """
+    result = copy.deepcopy(target)
+
+    # Sort changes so removals are processed last (to avoid index shifts in lists)
+    # and additions/modifications first.
+    additions = [c for c in changes if c.change_type == ChangeType.ADDED]
+    modifications = [c for c in changes if c.change_type == ChangeType.MODIFIED]
+    removals = [c for c in changes if c.change_type == ChangeType.REMOVED]
+
+    for change in modifications:
+        _apply_change(result, change)
+
+    for change in additions:
+        _apply_change(result, change)
+
+    # Process removals in reverse path order so list indices stay valid
+    for change in sorted(removals, key=lambda c: c.path, reverse=True):
+        _apply_removal(result, change)
+
+    return result
+
+
+def _parse_path(path: str) -> list[str | int]:
+    """Parse a dotted path with bracket indices into segments."""
+    segments: list[str | int] = []
+    current = ""
+    i = 0
+    while i < len(path):
+        ch = path[i]
+        if ch == ".":
+            if current:
+                segments.append(current)
+                current = ""
+        elif ch == "[":
+            if current:
+                segments.append(current)
+                current = ""
+            j = path.index("]", i)
+            segments.append(int(path[i + 1 : j]))
+            i = j
+        else:
+            current += ch
+        i += 1
+    if current:
+        segments.append(current)
+    return segments
+
+
+def _apply_change(obj: Any, change: Change) -> None:
+    """Set a value at the given change path."""
+    segments = _parse_path(change.path)
+    target = obj
+    for seg in segments[:-1]:
+        if isinstance(seg, int):
+            target = target[seg]
+        else:
+            target = target[seg]
+    last = segments[-1]
+    if isinstance(target, list) and isinstance(last, int):
+        if last >= len(target):
+            target.append(change.new_value)
+        else:
+            target[last] = change.new_value
+    else:
+        target[last] = change.new_value
+
+
+def _apply_removal(obj: Any, change: Change) -> None:
+    """Remove a value at the given change path."""
+    segments = _parse_path(change.path)
+    target = obj
+    for seg in segments[:-1]:
+        target = target[seg]
+    last = segments[-1]
+    if isinstance(target, list) and isinstance(last, int):
+        if last < len(target):
+            del target[last]
+    elif isinstance(target, dict) and isinstance(last, str):
+        target.pop(last, None)
